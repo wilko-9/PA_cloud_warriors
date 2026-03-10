294937fc0b12d283c7f059bc29df7554cb235bd9
Fedde-Pons <1116187@hr.nl>
2026-03-10 14:31:25 +0100

    Made quest-rewards branch up-to-date

--- alpha/obj/Debug/net10.0/alpha.AssemblyInfo.cs
+++ alpha/obj/Debug/net10.0/alpha.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("alpha")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0eb091fa86990b84e9c7cd741ea2d816b30c7c3c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da82dc1c6c20028a5fc84218fa4eac6c91222861")]
 [assembly: System.Reflection.AssemblyProductAttribute("alpha")]
 [assembly: System.Reflection.AssemblyTitleAttribute("alpha")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
