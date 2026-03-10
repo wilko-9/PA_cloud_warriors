44e7578c3f0d25509bae3f8c2146ea888b20e45f
WIlko <79004737+wilko-9@users.noreply.github.com>
2026-03-10 15:08:15 +0100

    combat

---
+++ alpha/Combat.cs
@@ -0,0 +1,79 @@
+namespace alpha;
+
+public class Combat
+{
+    Player PlayerCombat;
+    Monster MonsterCombat;
+    public Combat(Player player, Monster monster)
+    {
+        MonsterCombat = monster;
+        PlayerCombat = player;
+    }
+
+    private void AskPlayer()
+    {
+        Console.WriteLine("options: \n 1: attack");
+        string? option = Console.ReadLine();
+        if (option is not null)
+        {
+            if (int.TryParse(option, out int result))
+            {
+                if (result > 0)
+                {
+                    switch (result)
+                    {
+                        case 1:
+                            PlayerAttack();
+                            Console.WriteLine($"player {PlayerCombat.CurrentHitPoints}, monster {MonsterCombat.CurrentHitPoints}");
+                            break;
+                        default:
+                            Console.WriteLine("please try again");
+                            AskPlayer();
+                            break;
+                    }
+                }
+                if (result < 0)
+                {
+                   AskPlayer();
+                }
+            }
+        }
+        else
+        {
+            Console.WriteLine("please choose a number");
+        }
+
+    }
+
+    private void PlayerAttack()
+    {
+        MonsterCombat.CurrentHitPoints -= PlayerCombat.CurrentWeapon.MaximumDamage;
+    }
+
+    private void MonsterAttack()
+    {
+        PlayerCombat.CurrentHitPoints -= MonsterCombat.MaximumDamage;
+    }
+
+    public bool CombatMiniGamePlayerHasWon()
+    {
+        while (PlayerCombat.CurrentHitPoints > 0 && MonsterCombat.CurrentHitPoints > 0)
+        {
+            Console.Clear();
+            Console.WriteLine($"player {PlayerCombat.CurrentHitPoints}, monster {MonsterCombat.CurrentHitPoints}");
+            AskPlayer();
+            if (MonsterCombat.CurrentHitPoints < 0)
+            {
+                return true;
+            }
+            MonsterAttack();
+            if (PlayerCombat.CurrentHitPoints < 0)
+            {
+                return false;
+            }
+        }
+        // if something goes wrong the player loses
+        return false;
+    }
+
+}
